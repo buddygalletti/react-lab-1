c10f70205ab2a14dbf1f718f41c310e742a42efa
Buddy Galletti <buddygalletti@gmail.com>
2019-05-22 10:26:02 -0700

    Added solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// write your Color component here
+const Color = (props) => {
+  const color = props.color
+  const classString = (props.selectedColor === color)
+      ? color + ' selected'
+      : color
+  return (
+    <div className={ classString } onClick={() => props.selectColor(color)} />
+  )
+}
 
 class Picker extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      selectedColor: 'red'
+    }
+    // this.selectColor = this.selectColor.bind(this) // `bind` solution
+  }
+  // class property syntax solution, using arrow
+  selectColor = (colorName) => {
+    this.setState({
+      selectedColor: colorName
+    })
+  }
   render () {
+    const color = this.state.selectedColor
     return (
       <div id='container'>
         <div id='navbar'>
           <div>Currently selected: </div>
-          <div className='red'>red</div>
+          <div className={color}>{color}</div>
         </div>
         <div id='colors-list'>
-          {/* colors go here */}
+          <Color color="red" selectColor={this.selectColor} selectedColor={color} />
+          <Color color="blue" selectColor={this.selectColor} selectedColor={color} />
+          <Color color="green" selectColor={this.selectColor} selectedColor={color} />
         </div>
       </div>
     )
